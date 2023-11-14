ebe785f024e671474ba5b58474a269502431fb21
Roberto640Alvarado <00176620@uca.edu.sv>
2023-11-14 11:20:27 -0600

    💄Creando pagina web

---
+++ Client/smug-tickets/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar/NavbarHomepage.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+    
+      
+    </>
+  )
+}
+
+export default App
