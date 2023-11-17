9d3e5d157085bbc1c7a8757b08e9688f0680b416
EA03 <00175420@uca.edu.sv>
2023-11-16 19:58:39 -0600

    :lipstick:Agregar diseño de inicio de sesion con Google

--- Client/smug-tickets/src/App.jsx
+++ Client/smug-tickets/src/App.jsx
@@ -3,14 +3,15 @@ import './App.css'
 import Footer from './components/Footer/Footer.jsx'
 import Home from './pages/user/Home.jsx'
 
+/**
+ * Componente principal de la aplicación.
+ * @returns {JSX.Element} Elemento JSX que contiene el componente Home y el Footer.
+ */
 function App() {
-
-
   return (
     <>
       <Home />
-      <Footer /> 
-      
+      <Footer />
     </>
   )
 }
