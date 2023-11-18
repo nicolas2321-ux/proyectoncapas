54fe9da585cd596ad1a51c0f53d907f328fb2547
Lourdes Jiménez <00090220@uca.edu.sv>
2023-11-18 01:46:09 -0600

    :sparkles: El login funciona

--- Client/smug-tickets/src/pages/user/AuthGoogle.jsx
+++ Client/smug-tickets/src/pages/user/AuthGoogle.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { gapi } from 'gapi-script';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';  // Añade GoogleOAuthProvider aquí
 import logo from '../../assets/smug_ticket.png'
+import {jwtDecode} from 'jwt-decode'
+import authService from '../../services/Auth/AuthService';
+import context from '../../Context/UserContext';
 
 function AuthGoogle(props) {
   const clientID = "151373060419-hflbjm4m12o1odr0frs1v4ad7rvpael6.apps.googleusercontent.com";
@@ -15,6 +18,18 @@ function AuthGoogle(props) {
     gapi.load("client:auth2", start);
   }, []);
 
+  const handleLogin = async(data) =>{
+
+    const res = await authService.login(data.email);
+    if(res.status == 200){
+      context.login(data.email);
+      const id = await authService.verifyToken(res.data.content);
+      console.log(id);
+      console.log("Inicio de sesión exitoso!")
+    }else{
+      console.log("Error al iniciar sesión!")
+    }
+  }
   const onSuccess = (res) => {
     console.log(res);
   };
@@ -33,7 +48,9 @@ function AuthGoogle(props) {
             <img src={logo} alt="logo" className="w-64" />
           </div>
           <GoogleLogin
-            onSuccess={onSuccess}
+            onSuccess={(credentialResponse) => {
+              handleLogin(jwtDecode(credentialResponse.credential));
+            }}
             onFailure={onFailure}
             cookiePolicy={"single_host_policy"}
             className="w-full bg-blue-500 text-white rounded-md py-2 px-4 hover:bg-blue-600 transition duration-300"
