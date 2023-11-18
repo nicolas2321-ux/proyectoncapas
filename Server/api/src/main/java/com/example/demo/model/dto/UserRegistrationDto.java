c48e50c0a274009c82da3bd9fea8ab7a29ab5295
Lourdes Jiménez <00090220@uca.edu.sv>
2023-11-17 23:04:39 -0600

    :bug: Arreglé bug de registro

--- Server/api/src/main/java/com/example/demo/model/dto/UserRegistrationDto.java
+++ Server/api/src/main/java/com/example/demo/model/dto/UserRegistrationDto.java
@@ -9,13 +9,11 @@ public class UserRegistrationDto {
 	@NotEmpty
 	private String Username;
 	
-	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "Correo electrónico inválido")
+	
 	@NotEmpty
 	private String Email;
 	
 	@NotEmpty
-	 @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-     message = "La contraseña debe tener al menos 8 caracteres, 1 mayúscula, 1 minúscula, 1 dígito y 1 carácter especial")
 	private String Password;
 
 	@NotEmpty
