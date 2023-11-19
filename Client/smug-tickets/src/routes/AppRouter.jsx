9d471c77adf99dfd8ae1164d93d18e7c414b56d7
AlejandroOrteag17 <00166120@uca.edu.sv>
2023-11-19 00:47:00 -0600

    :sparkles: Visualización de todos los usuario

--- Client/smug-tickets/src/routes/AppRouter.jsx
+++ Client/smug-tickets/src/routes/AppRouter.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthGoogle from "../pages/user/AuthGoogle";
 import GoogleRegister from "../pages/user/GoogleRegister";
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import Home from "../pages/user/Home";
 import CreateEvent from "../pages/admin/CreatEvent";
-
+import UserManagement from "../pages/admin/UserManagement";
 
 export const AppRouter = () =>{
     return(
@@ -16,6 +15,7 @@ export const AppRouter = () =>{
                     <Route path="/login" element={<AuthGoogle />} />
                     <Route path="/register" element={<GoogleRegister />} />
                     <Route path="/create" element={<CreateEvent />} />
+                    <Route path="/allusers" element={<UserManagement />} />
                     
                     {/*<Route path="/register" element={<GoogleRegister />} />*/}
                 </Routes>
